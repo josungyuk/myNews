89a1cb349d29c65f4723b1f386b9f12ebaf4944c
josungyuk <50025489+josungyuk@users.noreply.github.com>
2026-01-14 16:39:20 +0900

    feat: DDD 구조 개선, news Entity 클래스 생성
    - DDD 구조 확인 후 개선 작업 수행. 기본적인 구조만 생성한 뒤 필요에 따라 수정될 예정
    
    - Entity 파일 및 router 폴더 생성. 용도와 역할에 맞게 구조화 진행
    
    - DB 연동 확인 후 서비스 안정화 및 고도화 설계 필요.

---
+++ app/modules/news/domain/entities/news.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, Text, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.common.db.base import Base
+
+class NewsEntity(Base):
+    __tablename__ = "news"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+    )
+
+    hash_id: Mapped[str] = mapped_column(
+        String(16),
+        nullable=False,
+    )
+
+    title: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    content: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    datetime: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
